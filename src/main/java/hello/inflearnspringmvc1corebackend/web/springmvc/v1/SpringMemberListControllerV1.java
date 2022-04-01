8b661604c9b38773ecbde922d000e38383f06496
6047198844 <jslee@miridih.com>
2022-04-01 12:40:52 +0900

    feat: add Spring mvc v1

---
+++ src/main/java/hello/inflearnspringmvc1corebackend/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,23 @@
+package hello.inflearnspringmvc1corebackend.web.springmvc.v1;
+
+import hello.inflearnspringmvc1corebackend.domain.member.Member;
+import hello.inflearnspringmvc1corebackend.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        final List<Member> members = memberRepository.findAll();
+        final ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
