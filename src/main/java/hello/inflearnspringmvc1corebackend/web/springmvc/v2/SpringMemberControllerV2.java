b93791df3025f1508741a44b8c1c6363d48459d4
6047198844 <jslee@miridih.com>
2022-04-01 12:47:50 +0900

    feat: add Spring mvc v2

---
+++ src/main/java/hello/inflearnspringmvc1corebackend/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,43 @@
+package hello.inflearnspringmvc1corebackend.web.springmvc.v2;
+
+import hello.inflearnspringmvc1corebackend.domain.member.Member;
+import hello.inflearnspringmvc1corebackend.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        final List<Member> members = memberRepository.findAll();
+        final ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
